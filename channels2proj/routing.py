4804bd100601e439f838cde09d7ae313bf7a888c
chitranjali <chitranjali189@gmail.com>
2018-07-06 13:22:33 +0530

    TickTock consumer working

--- channels2proj/routing.py
+++ channels2proj/routing.py
@@ -1,11 +1,11 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 
-from notifier.consumers import EchoConsumer
+from notifier.consumers import TickTockConsumer
 
 application = ProtocolTypeRouter({
     "websocket": URLRouter([
-        path("ws/", EchoConsumer),
+        path("ws/", TickTockConsumer),
     ])
 })
 
