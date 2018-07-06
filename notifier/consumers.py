07419b0b2cfa9703b232eaf2f42cb97e8f174c30
chitranjali <chitranjali189@gmail.com>
2018-07-06 14:09:17 +0530

    notifications popup in browser-complete

--- notifier/consumers.py
+++ notifier/consumers.py
@@ -1,3 +1,21 @@
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
+
+class NoseyConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        await self.channel_layer.group_add("gossip", self.channel_name)
+        print(f"Added {self.channel_name} channel to gossip")
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard("gossip", self.channel_name)
+        print(f"Removed {self.channel_name} channel to gossip")
+
+    async def user_gossip(self, event):
+        await self.send_json(event)
+        print(f"Got message {event} at {self.channel_name}")
+
+
 # from channels.consumer import AsyncConsumer
 #
 #
@@ -14,16 +32,16 @@
 #             "text": event["text"]
 #         })
 
-import asyncio
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
-
-
-class TickTockConsumer(AsyncJsonWebsocketConsumer):
-
-    async def connect(self):
-        await self.accept()
-        while 1:
-            await asyncio.sleep(1)
-            await self.send_json("tick")
-            await asyncio.sleep(1)
-            await self.send_json(".....tock")
+# import asyncio
+# from channels.generic.websocket import AsyncJsonWebsocketConsumer
+#
+#
+# class TickTockConsumer(AsyncJsonWebsocketConsumer):
+#
+#     async def connect(self):
+#         await self.accept()
+#         while 1:
+#             await asyncio.sleep(1)
+#             await self.send_json("tick")
+#             await asyncio.sleep(1)
+#             await self.send_json(".....tock")
